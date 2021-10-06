56971b359322c196ae9ae704031dcd5d8c53055c
deepraj14 <81950249+deepraj14@users.noreply.github.com>
2021-10-06 11:17:38 +0530

    chrome_extension

---
+++ script.js
@@ -0,0 +1,175 @@
+let a1=document.createElement('AUDIO')
+let a2=document.createElement('AUDIO')
+let a3=document.createElement('AUDIO')
+let a4=document.createElement('AUDIO')
+let a5=document.createElement('AUDIO')
+let a6=document.createElement('AUDIO')
+let a7=document.createElement('AUDIO')
+let a8=document.createElement('AUDIO')
+let a9=document.createElement('AUDIO')
+let a10=document.createElement('AUDIO')
+let a11=document.createElement('AUDIO')
+let a12=document.createElement('AUDIO')
+let a13=document.createElement('AUDIO')
+let a14=document.createElement('AUDIO')
+let a15=document.createElement('AUDIO')
+let a16=document.createElement('AUDIO')
+let a17=document.createElement('AUDIO')
+let a18=document.createElement('AUDIO')
+let a19=document.createElement('AUDIO')
+let a20=document.createElement('AUDIO')
+let a21=document.createElement('AUDIO')
+let a22=document.createElement('AUDIO')
+let a23=document.createElement('AUDIO')
+let a24=document.createElement('AUDIO')
+let a25=document.createElement('AUDIO')
+let a26=document.createElement('AUDIO')
+
+/*https://drive.google.com/file/d/1wh2JMmfTMNG4bai9_Bc5HlyJ4O7HY6BO/view?usp=sharing
+https://drive.google.com/file/d/1-3UlKxRsO-Y3W8RTomNLIFY0UZdgLkIT/view?usp=sharing
+https://drive.google.com/file/d/1brqgmcRhnFa_oqs89awVAQ8i6xhfHRJE/view?usp=sharing
+https://drive.google.com/file/d/1RLjvb0ie-eCqgNTCuun4GFNYMWXyJ5wK/view?usp=sharing
+https://drive.google.com/file/d/1tsDh2Y37B9HOCpu9EeEEgGhabETUFBpN/view?usp=sharing
+https://drive.google.com/file/d/1R7bRfPZ_DTsBx07--CR45PVkm7W4q5B6/view?usp=sharing
+https://drive.google.com/file/d/1q1rRJH2M22nGbaHlbTUJ6OwTTursQ6VJ/view?usp=sharing
+https://drive.google.com/file/d/19ZPepmWKHNhrSkkB7C3WmbFpuPHaRUgh/view?usp=sharing
+https://drive.google.com/file/d/1fM28sGmcyyec_oiqCDOhuCj1p_2rws4z/view?usp=sharing
+https://drive.google.com/file/d/1GtRI0WBoE338o4V0ywmn1cXj2XvkGFu1/view?usp=sharing
+https://drive.google.com/file/d/1lzKhX1SM-8Qsl1Fdpw7hiM56FofmmVcN/view?usp=sharing
+https://drive.google.com/file/d/11of4vIjHqqXqbl6GfTIbgqmYluloN-Ie/view?usp=sharing
+https://drive.google.com/file/d/10QC965Z4xo7R5lICg6-Wq7KwpaLvi3LE/view?usp=sharing
+https://drive.google.com/file/d/1enEv6w_9bGnaDIzG_vxY99mEuf_tM-I_/view?usp=sharing
+https://drive.google.com/file/d/1yYnkI7SUPmzTiGb0yvrQFP_ndjpaGK0Y/view?usp=sharing
+https://drive.google.com/file/d/1l4sUsUR5787zZlQ3VHkedjolDV8JJBTb/view?usp=sharing
+https://drive.google.com/file/d/1hLm-t6Iu-PU2MpyIC7QyA6RBu_RCp_ak/view?usp=sharing
+https://drive.google.com/file/d/1D9vLIUAq6AptrVpA5nLt7U5emJBlJG_F/view?usp=sharing
+https://drive.google.com/file/d/1G885CYvavCI-Jgejxn_5_I4wXjCcXVXJ/view?usp=sharing*/
+
+a1.setAttribute('src','http://docs.google.com/uc?export=open&id=1q7IOIKGfCYdVi5immLXrfZcsX0-GylvP')
+a2.setAttribute('src','http://docs.google.com/uc?export=open&id=1wh2JMmfTMNG4bai9_Bc5HlyJ4O7HY6BO')
+//a3.setAttribute('src','http://docs.google.com/uc?export=open&id=1-3UlKxRsO-Y3W8RTomNLIFY0UZdgLkIT')
+a4.setAttribute('src','http://docs.google.com/uc?export=open&id=1brqgmcRhnFa_oqs89awVAQ8i6xhfHRJE')
+a5.setAttribute('src','http://docs.google.com/uc?export=open&id=1RLjvb0ie-eCqgNTCuun4GFNYMWXyJ5wK')
+a6.setAttribute('src','http://docs.google.com/uc?export=open&id=1tsDh2Y37B9HOCpu9EeEEgGhabETUFBpN')
+a7.setAttribute('src','http://docs.google.com/uc?export=open&id=1R7bRfPZ_DTsBx07--CR45PVkm7W4q5B6')
+a8.setAttribute('src','http://docs.google.com/uc?export=open&id=1q1rRJH2M22nGbaHlbTUJ6OwTTursQ6VJ')
+a9.setAttribute('src','http://docs.google.com/uc?export=open&id=19ZPepmWKHNhrSkkB7C3WmbFpuPHaRUgh')
+a10.setAttribute('src','http://docs.google.com/uc?export=open&id=1fM28sGmcyyec_oiqCDOhuCj1p_2rws4z')
+a11.setAttribute('src','http://docs.google.com/uc?export=open&id=1GtRI0WBoE338o4V0ywmn1cXj2XvkGFu1')
+a12.setAttribute('src','http://docs.google.com/uc?export=open&id=1lzKhX1SM-8Qsl1Fdpw7hiM56FofmmVcN')
+a13.setAttribute('src','http://docs.google.com/uc?export=open&id=11of4vIjHqqXqbl6GfTIbgqmYluloN-Ie')
+a14.setAttribute('src','http://docs.google.com/uc?export=open&id=10QC965Z4xo7R5lICg6-Wq7KwpaLvi3LE')
+a15.setAttribute('src','http://docs.google.com/uc?export=open&id=1enEv6w_9bGnaDIzG_vxY99mEuf_tM-I_')
+a16.setAttribute('src','http://docs.google.com/uc?export=open&id=1yYnkI7SUPmzTiGb0yvrQFP_ndjpaGK0Y')
+a18.setAttribute('src','http://docs.google.com/uc?export=open&id=1l4sUsUR5787zZlQ3VHkedjolDV8JJBTb')
+a19.setAttribute('src','http://docs.google.com/uc?export=open&id=1hLm-t6Iu-PU2MpyIC7QyA6RBu_RCp_ak')
+a20.setAttribute('src','http://docs.google.com/uc?export=open&id=1D9vLIUAq6AptrVpA5nLt7U5emJBlJG_F')
+a21.setAttribute('src','http://docs.google.com/uc?export=open&id=1G885CYvavCI-Jgejxn_5_I4wXjCcXVXJ')
+a22.setAttribute('src','http://docs.google.com/uc?export=open&id=1Y8wl1HjUqjN1KhVb1Q8GbpVifYV5RfOL')
+a23.setAttribute('src','http://docs.google.com/uc?export=open&id=17oxnVir-RVx8389FNifEgzqqTiyp0FR5')
+a24.setAttribute('src','http://docs.google.com/uc?export=open&id=1rbz0LtDLSKbgjRruJ2jgYSvTsQXcif4v')
+a25.setAttribute('src','http://docs.google.com/uc?export=open&id=1h224FZK70SWhrM4mrTcJZmHTeKVOFEz7')
+a26.setAttribute('src','http://docs.google.com/uc?export=open&id=1q7IOIKGfCYdVi5immLXrfZcsX0-GylvP')
+
+
+/*https://drive.google.com/file/d/1Y8wl1HjUqjN1KhVb1Q8GbpVifYV5RfOL/view?usp=sharing
+https://drive.google.com/file/d/17oxnVir-RVx8389FNifEgzqqTiyp0FR5/view?usp=sharing
+https://drive.google.com/file/d/1rbz0LtDLSKbgjRruJ2jgYSvTsQXcif4v/view?usp=sharing
+https://drive.google.com/file/d/1h224FZK70SWhrM4mrTcJZmHTeKVOFEz7/view?usp=sharing
+https://drive.google.com/file/d/1q7IOIKGfCYdVi5immLXrfZcsX0-GylvP/view?usp=sharing
+//a1.setAttribute('src','a.mp3')*/
+
+
+a1.setAttribute("id",'aud1')
+
+console.log(a1)
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {      //a
+      a1.play();
+    }
+    else if(e.keyCode ==66){    //b
+        a2.play();
+    }
+    else if(e.keyCode ==67){    //c
+        //a3.play();
+        a6.play();
+    }
+    else if(e.keyCode ==68){   //d
+        a4.play();
+    }
+    else if(e.keyCode ==69){   //e
+        a5.play();
+    }
+    else if(e.keyCode ==70){    //f
+        a6.play();
+    }
+    else if(e.keyCode ==71){   //g
+        a7.play();
+    }
+    else if(e.keyCode ==72){  //h
+      //  a8.play();
+      a5.play();
+    }
+    else if(e.keyCode ==73){  //i
+       // a9.play();
+       a12.play();
+    }
+    else if(e.keyCode ==74){  //j
+        a10.play();
+    }
+    else if(e.keyCode ==75){   //k
+        a11.play();
+    }
+    else if(e.keyCode ==76){  //L
+        a12.play();
+    }
+    else if(e.keyCode ==77){  //M
+        a13.play();
+    }
+    else if(e.keyCode ==78){  //N
+        //a14.play();
+        a26.play();
+    }
+    else if(e.keyCode ==79){  //O
+        //a15.play();
+        a1.play();
+    }
+    else if(e.keyCode ==80){   //P
+       // a16.play();
+       a10.play();
+    }
+    else if(e.keyCode ==81){  //Q
+        a20.play();
+    }
+    else if(e.keyCode ==82){  //R
+      //  a18.play();
+      a21.play();
+    } 
+    else if(e.keyCode ==83){  //S
+        a19.play();
+    }
+    else if(e.keyCode ==84){ //T
+        a20.play();
+    }
+    else if(e.keyCode ==85){ //U
+        a21.play();
+    }
+    else if(e.keyCode ==86){  //V
+        a22.play();
+    }
+    else if(e.keyCode ==87){  //W
+        a23.play();
+    }
+    else if(e.keyCode ==88){  //X
+      //  a24.play(); 
+      a19.play();
+    }
+    else if(e.keyCode ==89){ //Y
+        a25.play();
+    }
+    else if(e.keyCode ==90){  //Z
+        a26.play();
+    }
+
+
+})
